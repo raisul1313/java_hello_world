13608c6e571df01b103d15440c6a9e265d8a3c3d
raisul1313 <raisul.islam1313@gmail.com>
2021-11-30 13:47:48 +0600

    Initial commit

---
+++ src/org/example/helloworld/Hello.java
@@ -0,0 +1,30 @@
+package org.example.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+
+        int number = -5;
+        System.out.println(number);
+
+        long myLong = 5;
+        System.out.println(myLong);
+
+        double myDouble = 5.5;
+        System.out.println(myDouble);
+
+        float myFloat = (float) 4.5;
+        System.out.println(myFloat);
+
+        char myChar = 'M';
+        System.out.println(myChar);
+
+        String name = "Raisul";
+        System.out.println(name);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+
+    }
+}
